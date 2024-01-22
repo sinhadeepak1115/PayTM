e14fca67cae07db29440f4a68d6bf04295efbb02
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-22 20:41:08 +0530

    more elegant code.

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,28 @@ mongoose.connect(process.env.CONNECTION_STRING);
 
 
 const schema = mongoose.Schema({
-  FirstName: String,
-  lastName: String,
-  password: String,
-
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+    maxlength: 20,
+    lowercase: true,
+    trim: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 4,
+  },
 })
 
 module.exports = mongoose.model("User", schema)
