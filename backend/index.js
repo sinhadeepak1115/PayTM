de397c95c2d1f642ff386f4ce186893565088fd7
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-01-23 00:26:12 +0530

    completed routing.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const rootRouter = require("./routes/index.js");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 
+app.use(express.json());
 
+const port = 3000;
 
+app.use("/api/v1", rootRouter);
+
+app.listen(port, () => {
+  console.log(`App Listening on port ${port}`)
+})
